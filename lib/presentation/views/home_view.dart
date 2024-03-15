69b2d42ef4e3357188427478eac58e602b4a1dd8
Syed Laiq Afzal <syedlaiqafzal37@gmail.com>
2024-03-16 03:21:44 +0500

    remove precacheimage

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,27 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/core/utils/app_assets.dart';
 import '../widgets/body/home_body.dart';
 import '../widgets/app_bar/home_app_bar.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  @override
-  void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
-    super.didChangeDependencies();
-    precacheImage(const AssetImage(AppAssets.nutriwestAppMockup), context);
-    precacheImage(const AssetImage(AppAssets.ethreadAppMockup), context);
-    precacheImage(const AssetImage(AppAssets.quranAppMockup), context);
-    precacheImage(const AssetImage(AppAssets.mahsaAppMockup), context);
-    precacheImage(
-        const AssetImage(AppAssets.footballUniverseAppMockuo), context);
-  }
 
   @override
   Widget build(BuildContext context) {
